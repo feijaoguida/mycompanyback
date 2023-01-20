1b5cfe9aa518ad18d26bebdff01bea2b84aaae6b
Roseweltty Guida <roseweltty.guida@squadra.com.br>
2023-01-20 08:49:29 -0300

    feat: Adjust model user

--- prisma/migrations/20230120104413_migrate_create_base/migration.sql
+++ prisma/migrations/20230120104413_migrate_create_base/migration.sql
@@ -3,6 +3,7 @@ CREATE TABLE "HUB_users" (
     "id" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
 
